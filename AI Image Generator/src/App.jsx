bbe1b69a4bbfb83ec07f1e02e595a1baf1f8389c
Daniel Okeke <danielekene68@gmail.com>
2025-05-28 14:54:01 +0100

    Initial Commit

---
+++ AI Image Generator/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+
+
+  return (
+    <>
+    <h1 className="text-red-500">hello world</h1>
+    <h2 className="text-blue-500">Welcome to the AI Image Generator</h2>
+
+
+    </>
+  )
+}
+
+export default App
